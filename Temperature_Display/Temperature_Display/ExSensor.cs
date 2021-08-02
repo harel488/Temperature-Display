15d2ef4ba409c525430a01f797c15186201555c6
harel488 <57444080+harel488@users.noreply.github.com>
2021-08-02 17:12:12 +0300

    intial

---
+++ Temperature_Display/Temperature_Display/ExSensor.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Temperature_Display
+{
+    class ExTempSensor
+    {
+        const double MAX_VAL=10.0;
+        const double MIN_VAL= 1.0;
+        const double MAX_TEMP = 90;
+        const double MIN_TMP = 25;
+        static double _partial = (MAX_TEMP - MIN_TMP) / (MAX_VAL - MIN_VAL) ; // Dividing the range of sensor values into 
+                                                                             //  the possible temperature range size.
+                                                                             
+        public double _currentValue;
+        
+        // Default Constructor  
+        public ExTempSensor()
+        {
+            _currentValue = 1;
+        }
+
+        /// <summary>
+        /// To simulate that the system is connected to a real sensor 
+        /// I generate a random value within the value range of the sensor
+        /// </summary>
+        public void MakeRandomVal()
+        {
+            Random rnd = new Random();
+            double val = rnd.NextDouble() * (MAX_VAL - MIN_VAL) + MIN_VAL;
+            _currentValue = val;
+        }
+
+        /// <summary>
+        /// converting the sensor value (range 1-10) to a Celsius value(25-90).
+        /// Multiplies the current value by the partial and adds the minimum value of the temperature 
+        /// (the value from which the temperature starts)
+        /// </summary>
+        /// <returns></returns>
+        public double ConvertToCelcius()
+        {
+            return ((_currentValue - 1) * _partial) + MIN_TMP;
+        }
+    }
+}
---
+++ Temperature_Display/Temperature_Display/MainWindow.xaml.cs
@@ -0,0 +1,94 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.IO;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+using System.Threading;
+
+namespace Temperature_Display
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DispatcherTimer timerDisp = new DispatcherTimer();
+        DispatcherTimer tempDisp = new DispatcherTimer();
+        int min = 0, sec = 0, milisec = 0;
+        const string TEMP_FILE_PATH = @"C:\Users\harel isaschar\source\repos\Temperature_Display\Temperature_Display\files\tempFile.txt";
+
+        ExTempSensor sens;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            sens = new ExTempSensor();
+            timerDisp.Interval = TimeSpan.FromMilliseconds(1); // dispatcher for timer label
+
+            tempDisp.Interval = TimeSpan.FromSeconds(30);     //  dispatcher for printing the temperature to screen
+                                                              //   and write the current value of the sensor to text file
+            timerDisp.Tick += OnTime;
+            tempDisp.Tick += TempDisplay;
+            timerDisp.Start();
+            tempDisp.Start();
+            tempLabel.Content = File.ReadAllText(TEMP_FILE_PATH) + " °C";
+
+        }
+
+
+        /// <summary>
+        /// replacing the content of tempLabel every 30 sec, and store it in text file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void TempDisplay(object sender, EventArgs e)
+        {
+            sens.MakeRandomVal();
+            File.WriteAllText(TEMP_FILE_PATH, String.Format("{0:0.00}", sens.ConvertToCelcius()));
+            string temp = File.ReadAllText(TEMP_FILE_PATH) + " °C";
+            tempLabel.Content = temp;
+        }
+
+
+
+
+
+
+        /// <summary>
+        /// Changing the timer content by miliseconds
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void OnTime(object sender, EventArgs e)
+        {
+            milisec++;
+            if (milisec == 60)
+            {
+                milisec = 0;
+                sec++;
+            }
+
+            if (sec == 60)
+            {
+                sec = 0;
+                min++;
+            }
+
+            timer.Content = string.Format("{0}:{1}:{2}", min.ToString().PadLeft(2, '0'), sec.ToString().PadLeft(2, '0'), milisec.ToString().PadLeft(2, '0'));
+        }
+    }
+}
+
+
+
